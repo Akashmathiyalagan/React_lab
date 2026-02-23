a31ae0fc447de43fb302e772c03c42d76bad59b0
Akash <166621275+Akashmathiyalagan@users.noreply.github.com>
2026-02-23 05:53:48 +0530

    Refactor Post component to display static content
    Removed the previous implementation that displayed a post based on its ID and replaced it with a static content display.

--- p10/src/Post.jsx
+++ p10/src/Post.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { posts } from './Home'
 
 function Post() {
-  const { id } = useParams()
-  const post = posts.find((p) => p.id === Number(id))
-
-  if (!post) {
-    return (
-      <div className="blog-container">
-        <p>Post not found</p>
-        <Link to="/" className="back">
-          ← Back to Home
-        </Link>
-      </div>
-    )
-  }
-
   return (
-    <div className="blog-container">
-      <Link to="/" className="back">
-        ← Back to Home
-      </Link>
-      <div className="post-card full">
-        <h2>{post.title}</h2>
-        <p>{post.content}</p>
-      </div>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum maxime iste magnam vero nihil aliquam mollitia asperiores nobis, laboriosam veritatis.</p>
     </div>
   )
 }
